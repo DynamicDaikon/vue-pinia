d805744c166dd0220093cc9774cabd190fcaf83d
DynamicDaikon <hietasoba@gmail.com>
2024-03-06 23:02:30 +0900

    add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
-<template>hello</template>
+<template>
+  <h1 class="flex">menu</h1>
+  <ul class="flex">
+    <li>Home</li>
+    <li>About</li>
+  </ul>
+</template>
 
 <script></script>
 
-<style></style>
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+
+ul li {
+  margin: 2rem;
+}
+</style>
